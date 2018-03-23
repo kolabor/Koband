b5dfe8b4c8289c7ff10d795d15d9798bca16807f
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-03-23 17:27:51 +0100

    fisnik

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ if ( !defined( 'KOLABOR_BAND_THEME_VERSION' ) ) {
  define ( 'KOLABOR_BAND_THEME_VERSION', $theme_data->get( 'Version' ) );
 } 
 
+
 /**
  * Implement the custom post types functions
  */
@@ -30,12 +31,37 @@ require 'custom/ko_band_album_custom_post_type.php';
 require 'custom/ko_band_the_band_custom_post_type.php';
 require 'custom/ko_band_media_custom_post_type.php';
 
-/*add_action( 'admin_enqueue_scripts', 'wpdocs_selectively_enqueue_admin_script' );
+// Load custom css script for admin dashboard
 
-function my_enqueue($hook) {
+function load_custom_wp_admin_style() {
 
-wp_enqueue_script( 'my_custom_script', plugin_dir_url( __FILE__ ) . '/admin/ko_band_admin.js', array(), '1.0' );
-	
-} */
+        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/admin/ko_band_admin.css', false, '1.0.0' );
+        wp_enqueue_style( 'custom_wp_admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
 
+// Load custom javascript file for admin dashboard
 
+function load_custom_wp_admin_script() {
+
+        wp_register_script( 'custom_wp_admin_js', get_template_directory_uri() . '/admin/ko_band_admin.js', false, '1.0.0' );
+        wp_enqueue_script( 'custom_wp_admin_js' );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_script' );
+
+// Theme Support
+
+function koband_theme_support () {
+
+	// Add theme support thumnails
+
+	add_theme_support('post-thumbnails');
+
+	// Add theme support Post Format Support
+
+	add_theme_support('post-formats', array('aside', 'gallery', 'link'));
+}
+
+add_action('after_setup_theme', 'koband_theme_support');
+
+?>
