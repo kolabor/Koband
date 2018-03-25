fc000c4eee956b09715400cef25247b859545f42
Axheladini <agonxheladini001@gmail.com>
2018-03-25 23:25:34 +0200

    Repeatable boxes

--- custom/ko_band_slides_custom_post_types.php
+++ custom/ko_band_slides_custom_post_types.php
@@ -1,5 +1,12 @@
-<?php 
-
+<?php
+/**
+ * Koband Slides custom post type and metabox creation file!
+ *
+ *
+ * @package Wordpress 
+ * @subpackage Koband
+ * @since Koband 1.0
+ */
 
 $args = array(
     'labels'  =>  array(
