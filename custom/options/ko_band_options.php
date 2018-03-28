58f52c8fbb52d2f5876710ad67873f9ddff3c355
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-03-28 10:28:40 +0200

    rilinda 28.03.2018

--- custom/options/ko_band_options.php
+++ custom/options/ko_band_options.php
@@ -13,14 +13,14 @@ function ko_band_customize_register( $wp_customize )
 {
    //All our sections, settings, and controls will be added here
   	$wp_customize->add_section('ko_band_colors', array(
-        'title' => __ ('Colors', 'ko_band'),
+        'title' => __ ('Colors', 'koband'),
         'description' => 'Modify the theme colors'
     ));
     $wp_customize->add_setting('background_color', array(
         'default' => '#fff',
     ));
     $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize,'background_color', array(
-        'label' => __('Edit Background Color', 'ko_band'),
+        'label' => __('Edit Background Color', 'koband'),
         'section' => 'ko_band_colors',
         'settings' => 'background_color'
     )));
