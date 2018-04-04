f4109f9d9e4c6e91556b5878d420ba98c2e0f607
Axheladini <agonxheladini001@gmail.com>
2018-04-04 09:35:24 +0200

    before pulling

--- style/ko_band_dynamic.css.php
+++ style/ko_band_dynamic.css.php
@@ -1,20 +1,25 @@
 <?php
   header('Content-type: text/css');
   include_once("{$_SERVER['DOCUMENT_ROOT']}/wp-load.php"); 
+
+  $main_font = get_theme_mod( 'ko_band_general_font_selector' );
 ?>
 
-<?php 
 
+@import url('https://fonts.googleapis.com/css?family=<?php echo $main_font; ?>');
+
+
+
+<?php 
+	
 $main_theme_first_color = get_theme_mod( 'ko_band_main_color' );
 $main_theme_second_color = get_theme_mod( 'ko_band_second_color' );
 $main_theme_third_color = get_theme_mod( 'ko_band_third_color' );
 ?>
 
-/* Menu item colors */
-
 body
 {
-  background-color: <?php echo $main_theme_first_color; ?> !important;
+  font-family: <?php echo $main_font ?>;
 }
 
 
