1f06a9acde20561ae66ba3fe54c1d3998713ce1f
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-04-05 11:28:48 +0200

    R 05.04.2018

--- custom/templates/media-template.php
+++ custom/templates/media-template.php
@@ -29,15 +29,69 @@ get_header();?>
  $media_posts = new WP_Query( $args_media );
  $total = $media_posts->found_posts; 
 
+if($total>0):
  if ( $media_posts->have_posts() ) : 
  	//start loop
 	 while ( $media_posts->have_posts() ) : $media_posts->the_post(); 
 
-		//$post_id = get_the_ID() ?>
-			<a href="<?php the_permalink();?>"><?php the_tittle();?></a>
-		<?php
+		$post_id = get_the_ID();
+
+        $all  = get_post_meta($post_id);
+
+		//$images = get_post_meta("vdw_gallery_id", $post_id);
+//		$videos = get_post_meta("ko_band_repetable_video_field", $post_id);
+	
+
+
+
+/*
+ 		foreach($images as $key=>$val)
+       {
+             echo $key . ' show: ' . $val . '<br/>';
+       };
+		 foreach($videos as $key=>$val)
+       {
+             echo $key . ' show: ' . $val . '<br/>';
+       };*/
+		echo "<pre>";
+		print_r($all);
+		echo "</pre>";
+
+       /* echo "<pre>";
+		print_r($images['vdw_gallery_id'][0]);
+		echo "</pre>";
+
+		 echo "<pre>";
+		print_r($videos['ko_band_repetable_video_field'][0]);
+		echo "</pre>";*/
+
+
+		/*foreach($all as $key=>$val)
+       {
+             echo $key . ' : ' . $val[0] . '<br/>';
+       }*/
+
+      
+  
+
+			/*<!--<a href="<?php the_permalink();?>"><?php the_title();?></a>
+		
+			<li>	<?php get_post_meta( "vdw_gallery_id", $post_id );
+	 				//echo $fieldd_bank;?>
+	 				
+	 		</li>
+	 	
+	 		<li>	<?php get_post_meta( "link[]", $post_id );
+	 			//	echo $fieldd_kategory;?>
+	 				 
+
+	 		</li>
+	 		<li>	<?php get_post_meta( "select[]", $post_id );?> -->
+*/
 	 endwhile; // end of the loop. 
 endif;
+endif;
 
- 
+
  ?>
+
--- custom/templates/tour-template.php
+++ custom/templates/tour-template.php
@@ -12,10 +12,30 @@
  * @since Koband 1.0
  */
 
-wp_head(); ?> <h1>Tour Temp</h1>
-<?php 
+get_header(); ?> <h1>Tour Temp</h1>
+<?php
 
 
 
+    $args_tour = array
+    (		
+	 	 'post_type' => 'tour',   
+		 'post_staus'=> 'publish',
+		 'posts_per_page' => -1
+	);
 
+ $tour_posts = new WP_Query( $args_tour );
+ $total = $tour_posts->found_posts; 
+
+ if ( $tour_posts->have_posts() ) : 
+ 	//start loop
+	 while ( $tour_posts->have_posts() ) : $tour_posts->the_post(); 
+
+		//$post_id = get_the_ID() ?>
+			<a href="<?php the_permalink();?>"><?php the_title();?></a>
+		<?php
+	 endwhile; // end of the loop. 
+endif;
+
+ 
  ?>
