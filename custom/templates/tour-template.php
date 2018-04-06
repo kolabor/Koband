e40721d821c9cfff9552fd558dbd12a2641b23cf
Axheladini <agonxheladini001@gmail.com>
2018-04-06 18:20:51 +0200

    AX fixes

--- custom/templates/tour-template.php
+++ custom/templates/tour-template.php
@@ -25,82 +25,49 @@ get_header(); ?> <h1>Tour Temp</h1>
 	);
 
  $tour_posts = new WP_Query( $args_tour );
- $total = $tour_posts->found_posts; 
+
+
+
 
  if ( $tour_posts->have_posts() ) : 
  	//start loop
+?>
+<div class='tour_holder'>
+<?php
+
 	 while ( $tour_posts->have_posts() ) : $tour_posts->the_post(); 
 
-		$post_id = get_the_ID();
-/*$all  = get_post_meta($post_id);
-		echo "<pre>";
-		print_r($all);
-	echo "</pre>";*/
-
-		//$post_id = get_the_ID() ?>
-			<a href="<?php the_permalink();?>"><?php the_title();?></a>
+		$post_id = get_the_ID();  ?>
 			
 
 		<?php 
-			 the_post_thumbnail(array(200,200));
-
-		 $tour_date = get_post_meta( $post_id, 'ko_band_tour_date', false );
-		 foreach ($tour_date as $key => $value_date) {
-
-		 	print_r( $value_date);
-		 	# code...
-		 }
-
-		  $tour_country = get_post_meta($post_id, "ko_band_tour_country", false );
-		   foreach ($tour_country as $key => $value_country) {
-
-		 	print_r( $value_country);
-		 	# code...
-		 }
-
-		  $tour_city = get_post_meta($post_id, "ko_band_tour_city", false );
-		  foreach ($tour_city as $key => $value_city) {
-
-		 	print_r( $value_city);
-		 	# code...
-		 }	
-
+			 //the_post_thumbnail(array(200,200));
+         $tour_date = get_post_meta( $post_id, 'ko_band_tour_date', false );
+		 $tour_country = get_post_meta($post_id, "ko_band_tour_country", false );
+		 $tour_city = get_post_meta($post_id, "ko_band_tour_city", false );
 		 $tour_address = get_post_meta($post_id, "ko_band_tour_address", false );
-		 foreach ($tour_address as $key => $value_address) {
-
-		 	print_r( $value_address);
-		 	# code...
-		 }
-
 		 $tour_zipcode = get_post_meta($post_id,  "ko_band_tour_zipCode", false );
-		 foreach ($tour_zipcode as $key => $value_zipcode) {
-
-		 	print_r( $value_zipcode);
-		 	# code...
-		 }
-
 		 $tour_venuename = get_post_meta($post_id,  "ko_band_tour_venue_name", false );
-		 foreach ($tour_venuename as $key => $value_venuename) {
-
-		 	print_r( $value_venuename);
-		 	# code...
-		 }
 		 $tour_ticket = get_post_meta($post_id,  "ko_band_tour_ticket", false );
-		 foreach ($tour_ticket as $key => $value_ticket) {
-
-		 	print_r( $value_ticket);
-		 	# code...
-		 }
-
 		 $tour_ticketlink = get_post_meta($post_id, "ko_band_tour_ticket_link", false );
-		 foreach ($tour_ticketlink as $key => $value_ticketlink) {
-
-		 	print_r( $value_ticketlink);
-		 	# code...
-		 }
-
-	
+         ?>
+            <div class="tour_row">
+            	<a href="<?php the_permalink();?>"><?php the_title();?></a><br>
+                <?php echo  $tour_date[0]; ?><br>
+                <?php echo  $tour_country[0]; ?><br>
+                <?php echo  $tour_city[0]; ?><br>
+                <?php echo  $tour_address[0]; ?><br>
+                <?php echo  $tour_zipcode[0]; ?><br>
+                <?php echo  $tour_venuename[0]; ?><br>
+                <?php echo  $tour_ticket[0]; ?><br>
+                <?php echo  $tour_ticketlink[0]; ?><br>
+            </div>
+         <?php 
 	 endwhile; // end of the loop. 
+?>
+</div>;
+
+<?php
 endif;
 
  
