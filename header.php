f0e03d46f30a7375ae542e0e58d475bbea05df7f
theados <admir.ibraimi@kolabor.net>
2018-04-09 10:48:29 +0200

    adooo

--- header.php
+++ header.php
@@ -27,27 +27,9 @@ wp_head(); ?>
  </div> <!-- Here ends header-main div -->
 </header>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-   <script src="resources/js/script.js"></script>
-   <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
-         <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
-         <link rel="stylesheet" type="text/css" href="resources/css/style.css">
-         <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
-         <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
-         <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,900,300italic' rel='stylesheet' type='text/css'>
- <title><?php _e('Këpurdhat', 'koband');?></title>
-</head>
-<body>
- <header>
+<body>   
 
-   
-<!--<div class="wrap">-->
-   <nav>
+  <!-- <nav>
      <div class="row clearfix menu">
                     <img src="resources/img/logo-font.gif" alt="Koband-logo" class="logo">
       <ul class="main-nav ">
@@ -75,51 +57,28 @@ wp_head(); ?>
                     </ul>
                     <a class="mobile-nav-icon"><i class="ion-navicon-round"></i></a>
                    </div>
-    </nav>
-         <video autoplay muted loop id="myVideo">
-            <source src="resources/Kepurdhat-UneRabaBota.mp4" type="video/mp4">
-          </video> 
-        
+    </nav>-->         
+          <nav>
+              <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                      <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+          </ul>
 
-
-								 <!-- <div id="arrow-left" class="arrow"></div>
-								  <div id="slider">
-								    <div class="slide slide1">
-								    	
-								    </div>
-								    <div class="slide slide2">
-								      <div class="slide-content">
-								        
-								      </div>
-								    </div>
-								    <div class="slide slide3">
-								      <div class="slide-content">
-								        
-								      </div>
-								    </div>
-								  </div>
-								  <div id="arrow-right" class="arrow"></div>
-								</div>-->
-	</header>
-
-
-         <!-- <div id="arrow-left" class="arrow"></div>
-          <div id="slider">
-            <div class="slide slide1">
-             
-            </div>
-            <div class="slide slide2">
-              <div class="slide-content">
-                
-              </div>
-            </div>
-            <div class="slide slide3">
-              <div class="slide-content">
-                
-              </div>
-            </div>
-          </div>
-          <div id="arrow-right" class="arrow"></div>
-        </div>-->
- </header>
-
+          </nav>
