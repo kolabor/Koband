cfd5366aad8c8afb363efc8a92aa3d2c1c6c5c26
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-04-09 09:18:05 +0200

    fisnik

--- custom/templates/media-template.php
+++ custom/templates/media-template.php
@@ -26,12 +26,14 @@ get_header();?>
 
 
     $media_posts = new WP_Query($args_media);
-    $total = $media_posts->found_posts;
     if ($media_posts->have_posts() ) : 
  
 	while ( $media_posts->have_posts() ) : $media_posts->the_post();
-		$post_id = get_the_ID(); ?>
-
+		$post_id = get_the_ID(); 
+		$all  = get_post_meta($post_id);
+		echo "<pre>";
+		print_r($all);
+		echo "</pre>";?>
 
 		<a href="<?php the_permalink();?>"><?php the_title();?><?php the_post_thumbnail(array(200,200));?><?php the_content();?></a>
 <?php
@@ -40,7 +42,8 @@ get_header();?>
 		$media_gallery = get_post_meta($post_id, 'vdw_gallery_id', false);
 		foreach ($media_gallery as $key => $value_gallery) {
 			
-			var_dump($value_gallery) ;
+			echo '<img src="'.$value_gallery.'" alt="">';
+			
 		} 
 
 		$media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false);
