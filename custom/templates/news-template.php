5751e1616a670333b123c2aa26527c329af25a36
theados <admir.ibraimi@kolabor.net>
2018-04-13 12:04:05 +0200

    ado113

--- custom/templates/news-template.php
+++ custom/templates/news-template.php
@@ -11,17 +11,20 @@
  * @subpackage Koband
  * @since Koband 1.0
  */?>
-
-	<div class="row">
-	<h1 class="display-1">News</h1>
+ <div class="row">
+ 	<div class="row koband_title_news">
+ 	<div class="container">
+	<h1>News</h1>
 	</div>
-	<div class="container koband_post_container">
+	</div>
+	<div class="koband_post_container">
+	<div class="container">
 			<?php if ( have_posts() ) : ?>
 					<!-- start loop --> 							
 				<div class="row koband_post_news">
 					<?php while ( have_posts() ) : the_post(); ?>
 						<div class="col-sm-4">
-								<div id="news-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></div>
+								<div id="news-title"><h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2></div>
 								<a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(300,300)); ?></a>
 								<div id="news-excerpt"><?php the_excerpt(); ?></div>
 								<a class="read_more" href="<?php the_permalink();?>"><?php _e('Continue reading -->', 'koband'); ?></a>
@@ -38,5 +41,7 @@
 	<a class="btn-koband-load koband_load_more" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>"><span class="koband-loading">Loading...</span>
 	<span class="text">Load more</span></a>
 
+</div>
+</div>
 
 
