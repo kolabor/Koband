470d2e277cae6883076378299a34a1d3baac0df9
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-04-16 17:39:21 +0200

    rilinda 16.04

--- custom/templates/contact-template.php
+++ custom/templates/contact-template.php
@@ -19,7 +19,6 @@ get_header();?>
 <div class="album py5 bg-light" id="News">
     <div class="container"> <h1>Contact Us </h1>
         <div class="row">
-        
 
 <?php
 $google_map = get_theme_mod('ko_band_googlemap', false );
@@ -31,33 +30,74 @@ $contact_phone = get_theme_mod('ko_band_phone', false );
 ?>
 	
  		<div class="col-sm-4">
-        <?php _e('Country Name: ', 'koband'); ?>
-	        <?php echo $contact_country; ?></br>
+            <?php _e('Country Name: ', 'koband'); ?>
+	           <?php echo $contact_country; ?></br>
 	    
-	   <?php _e('City Name: ', 'koband'); ?>
-        	<?php echo $contact_city; ?></br>
+	        <?php _e('City Name: ', 'koband'); ?>
+        	   <?php echo $contact_city; ?></br>
 
-   		<?php _e('Address: ', 'koband'); ?>
-			<?php echo $contact_address;  ?></br>
+   		    <?php _e('Address: ', 'koband'); ?>
+		       <?php echo $contact_address;  ?></br>
 
-		<?php _e('Email: ', 'koband'); ?>
-      		<?php echo $contact_email; ?></br>
+		    <?php _e('Email: ', 'koband'); ?>
+      		    <?php echo $contact_email; ?></br>
 
-       <?php _e('Phone Number: ', 'koband'); ?> 
-       		<?php echo $contact_phone; ?></br>
+            <?php _e('Phone Number: ', 'koband'); ?> 
+       		   <?php echo $contact_phone; ?></br>
         </div>
-
        
         <div class="col-sm-8"><?php _e('Google Map: ', 'koband'); ?> </br>
-       		<?php echo $google_map; ?>  </br>
-            
+       		<?php echo $google_map; ?>  </br> 
         </div>             
+    </div>
+    <!-- Contact with Map - END -->
+    <div class="row">
+    
+<?php
+//Social Network start here
+$facebook = get_theme_mod( 'ko_band_facebook_social_media' , false);
+$twitter = get_theme_mod( 'ko_band_twitter_social_media' , false);
+$instagram = get_theme_mod( 'ko_band_insagram_social_media' , false);
+$googleplus= get_theme_mod( 'ko_band_googleplus_social_media' , false);
+
+$youtube= get_theme_mod( 'ko_band_youtube_social_media' , false);
+$github= get_theme_mod( 'ko_band_github_social_media' , false);
+$soundcloud= get_theme_mod( 'ko_band_soundcloud_social_media' , false);
+?>
+        <div class="col-sm-4"></div>
+  
+        <div class="col-sm-8">
+          
+         
+                <?php if(isset($facebook[0])) { echo $facebook;} ?>
+           
+                <?php if(isset($twitter[0])) { echo  $twitter; } ?>
+           
+                <?php if(isset($instagram[0])) { echo  $instagram; } ?>
+           
+                <?php if(isset($googleplus[0])) { echo  $googleplus; } ?>
+           
+          
+                <?php if(isset($youtube[0])) { echo  $youtube; } ?>
+        
+           
+                <?php if(isset($spotify[0])) { echo  $spotify; } ?>
+            
+                <?php if(isset($soundcloud[0])) { echo  $soundcloud; } ?>
+           
+                <?php if(isset($bandcamp[0])) { echo  $bandcamp; } ?>
+            
+                <?php if(isset($deezer[0])) { echo  $deezer; } ?>
+            
+                <?php if(isset($apple[0])) { echo  $apple; } ?>
+        </div>
    </div>
 </div>
 </div>
-<!-- Contact with Map - END -->
-
-<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<?php                     
+    //Social Network end here
+?>
+<script src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d11858.21723189105!2d20.968557999999998!3d42.0098398!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smk!4v1523891538553"></script>
 
 <script type="text/javascript">
     jQuery(function ($) {
@@ -95,63 +135,5 @@ $contact_phone = get_theme_mod('ko_band_phone', false );
         padding: 10px;
     }
 </style>
-
-
-
-<?php
-//Social Network start here
-$facebook = get_theme_mod( 'ko_band_facebook_social_media' , false);
-$twitter = get_theme_mod( 'ko_band_twitter_social_media' , false);
-$instagram = get_theme_mod( 'ko_band_insagram_social_media' , false);
-$googleplus= get_theme_mod( 'ko_band_googleplus_social_media' , false);
-$linkedin= get_theme_mod( 'ko_band_linkedin_social_media' , false);
-$youtube= get_theme_mod( 'ko_band_youtube_social_media' , false);
-$pinterest= get_theme_mod( 'ko_band_pinterest_social_media' , false);
-$github= get_theme_mod( 'ko_band_github_social_media' , false);
-$soundcloud= get_theme_mod( 'ko_band_soundcloud_social_media' , false);
-?>
- <div class="container">
-    <div class="row-socialmedia"> 
-    
-        <div class="col-sm"><?php _e('Facebook', 'koband'); ?></div>
-            <?php if(isset($facebook[0])) { echo $facebook;} ?><br>
-
-   
-        <div class="col-sm"><?php _e('Twitter', 'koband'); ?></div>
-            <?php if(isset($twitter[0])) { echo  $twitter; } ?><br>
-
-        <div class="col-sm"><?php _e('Istagram', 'koband'); ?></div>
-            <?php if(isset($instagram[0])) { echo  $instagram; } ?><br>
-
-        <div class="col-sm"><?php _e('Googleplus', 'koband'); ?></div>
-            <?php if(isset($googleplus[0])) { echo  $googleplus; } ?><br>
-
-        <div class="col-sm"><?php _e('Linkedin', 'koband'); ?></div>
-            <?php if(isset($linkedin[0])) { echo  $linkedin; } ?><br>
-
-        <div class="col-sm"><?php _e('Youtube', 'koband'); ?></div>
-            <?php if(isset($youtube[0])) { echo  $youtube; } ?><br>
-
-        <div class="col-sm"><?php _e('Pinterest', 'koband'); ?></div>
-            <?php if(isset($pinterest[0])) { echo  $pinterest; } ?><br>
-
-        <div class="col-sm"><?php _e('Spotify', 'koband'); ?></div>
-            <?php if(isset($spotify[0])) { echo  $spotify; } ?><br>
-
-        <div class="col-sm"><?php _e('Soundcloud', 'koband'); ?></div>
-            <?php if(isset($soundcloud[0])) { echo  $soundcloud; } ?><br>
-
-        <div class="col-sm"><?php _e('Bandcamp', 'koband'); ?></div>
-            <?php if(isset($bandcamp[0])) { echo  $bandcamp; } ?><br>
-
-        <div class="col-sm"><?php _e('Deezer', 'koband'); ?></div>
-            <?php if(isset($deezer[0])) { echo  $deezer; } ?><br>
-
-        <div class="col-sm"><?php _e('Apple', 'koband'); ?></div>
-            <?php if(isset($apple[0])) { echo  $apple; } ?><br>
-</div> 
-</div>  
-</div>                                   
-  <?php                     
-    //Social Network end here
-?>
+                                 
+  
