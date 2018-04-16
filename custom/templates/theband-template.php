76581d3ff6cbea10f8f96a605a4778dafbd1b165
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-04-16 16:53:14 +0200

    rilinda 16.04

--- custom/templates/theband-template.php
+++ custom/templates/theband-template.php
@@ -12,55 +12,44 @@
  * @since Koband 1.0
  */
 
-get_header(); ?> <div class="theband"><h1>The Band Temp</h1>
+get_header(); ?>
+<div class="album py5 bg-theband" id="theband">
+ 	<div class="container">
+ 		<div class="row">
+ 			<h1>The Band Temp</h1>
+		</div>
+	</div>
+	
+			<?php $args_theband = array(		
+			 	'post_type' => 'theband',   
+				'post_staus'=> 'publish',
+				'posts_per_page' => -1
+			);
+			$theband_posts = new WP_Query( $args_theband );?>
+			
+	<div class="container">
+		<div class="row koband_post_theband">
+			<div class="col-sm-4">
+				<!--<h2>test</h2>-->
+				<?php 
+				$theband_bio = get_theme_mod('ko_band_the_band_biography');
+				echo $theband_bio;
+				?>
+			</div>
 
-<?php
-
-
-    $args_theband = array
-    (		
-	 	 'post_type' => 'theband',   
-		 'post_staus'=> 'publish',
-		 'posts_per_page' => -1
-	);
-
- $theband_posts = new WP_Query( $args_theband );
- $total = $theband_posts->found_posts; 
-
- if ( $theband_posts->have_posts() ) : 
- 	//start loop
-	 while ( $theband_posts->have_posts() ) : $theband_posts->the_post(); 
-
-
-		$post_id = get_the_ID();
-/*$all  = get_post_meta($post_id);
-		echo "<pre>";
-		print_r($all);
-	echo "</pre>";*/
-
-		//$post_id = get_the_ID() ?>
-<div class="test">
-<h2> Band Member name: <a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-
-<?php the_content();?>
-
-<?php
-		 $theband_bio = get_post_meta( $post_id, 'ko_band_the_band_bio', false );
-		 foreach ($theband_bio as $key => $value_bio) {
-		 	?>
-		 	<div class="ado">Role: 
-		 	<?php
-		 	print_r( $value_bio);
-		 	# code...
-		 	?> </div> <?php 
-		 }
-
-	 endwhile; // end of the loop. 
-endif;
-
- 
- ?></div>
- <div class="img">
- <?php the_post_thumbnail(array(200,200));?> 
- </div>
- </div>
+			<div class="col-sm-8">		
+			<?php 
+ 				if ( $theband_posts->have_posts() ) : 
+ 				//start loop
+					while ( $theband_posts->have_posts() ) : $theband_posts->the_post(); ?>
+					<?php $post_id = get_the_ID(); ?> 
+					<?php the_title(); the_content();?></br>
+						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(200,200)); ?></a></br>
+					<?php $theband_role = get_post_meta( $post_id, 'ko_band_the_band_bio', false ); ?>
+   			 		<?php if(isset($theband_role[0])) 		 { echo  $theband_role[0]; } ?></br></br></br>
+      				<?php endwhile;
+      			endif;?>
+   			</div>
+		</div>
+	</div>
+</div>
