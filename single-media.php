adc9ac9f267549f9ca7eb4ec55d080650acef870
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-04-19 16:34:12 +0200

    fisnik

--- single-media.php
+++ single-media.php
@@ -17,83 +17,66 @@ get_header(); ?>
 if (have_posts() ) : 
 	
  	//start loop ?>
+<div class='media_holder'>
+	<?php  while ( have_posts() ) : the_post(); 
+	$post_id = get_the_ID(); ?>
+	<div id="media-photo"><a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(1024,800)); ?></a>
+		<div class="container">
+		<h1><div id="single-media-title"><?php the_title();?></div></h1>
+			<div class="row">
+				<div class="col-sm">
+					<div id="single-media-content"><?php the_content(); ?></div>
+				</div>
+			</div>
+	<?php 
+	$media_gallery = get_post_meta($post_id, 'vdw_gallery_id', false);
+	$media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); ?>
+			<div class="row">
+			<?php foreach ($media_video_gallery[0] as  $value_video_gallery) { ?>
+				<div class="col-sd-4"> 
+<!--================================================================================================================
+						// Checking which iframe to use from the select box at backed :)
+=================================================================================================================--> 	
+		            <?php
+		            $video_type = $value_video_gallery['select'];
+					if(isset($video_type) && $video_type == "option1"){	
+						$value_video_gallery['link'] = 'https://www.youtube.com/embed/' . $value_video_gallery['link'];?>		
+						<iframe width="370" height="265" src="<?php echo $value_video_gallery['link']?>"></iframe> 
+					<?php }
 
+					elseif(isset($video_type) && $video_type == "option2"){
+						$value_video_gallery['link'] = 'https://player.vimeo.com/video/' . $value_video_gallery['link']; ?>
+						<iframe width="370" height="265" src="<?php echo $value_video_gallery['link']?>"></iframe>
+					<?php }
 
-	<div class='media_holder'>
+					elseif(isset($video_type) && $video_type == "option3"){
+						$value_video_gallery['link'] = '//www.dailymotion.com/embed/video/' . $value_video_gallery['link']; ?>
+						<iframe width="370" height="265" src="<?php echo $value_video_gallery['link']?>"></iframe>
+					<?php }
 
-		<?php  while ( have_posts() ) : the_post(); 
-				$post_id = get_the_ID(); ?>
-
-				<div id="media-photo"><a href="<?php the_permalink();?>"><?php the_post_thumbnail(array(1024,800)); ?></a>
-				<div class="container">
-				<h1><div id="single-media-title"><?php the_title();?></div></h1>
-						<div class="row">
-							<div class="col-sm">
-								<div id="single-media-content"><?php the_content(); ?></div>
-							</div>
-						</div>
-				 
-						<?php $media_gallery = get_post_meta($post_id, 'vdw_gallery_id', false);
-						$media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); 
-						$video_galery_options = ko_band_get_video_options(); 
-						print_r($video_galery_options);?> 
-
-						<div class="row">
-							<?php foreach ($media_video_gallery[0] as  $value_video_gallery) { ?>
-							<div class="col-sd-4"> <?php
-//================================================================================================================
-							// Checking which iframe to use from the select box at backed :)
-//================================================================================================================  
-							if(isset($video_galery_options) && $video_galery_options == 'option1'){ 
-									
-								$value_video_gallery['link'] = 'https://www.youtube.com/embed/' . $value_video_gallery['link']; ?>
-										
-								<iframe width="420" height="315"
-									src="<?php echo $value_video_gallery['link']?>">
-								</iframe> 
-
-							<?php	}
-							elseif(isset($video_galery_options) && $video_galery_options == 'option2'){
-							
-								$value_video_gallery['link'] = 'https://player.vimeo.com/video/' . $value_video_gallery['link']; ?>
-
-								<iframe width="420" height="315"
-									src="<?php echo $value_video_gallery['link']?>">
-								</iframe>
-
-							<?php }
-							elseif(isset($video_galery_options) && $video_galery_options == 'option3'){
-							
-								$value_video_gallery['link'] = '//www.dailymotion.com/embed/video/' . $value_video_gallery['link']; ?>
-
-								<iframe width="420" height="315"
-									src="<?php echo $value_video_gallery['link']?>">
-								</iframe>
-							<?php }
-							else {
-								echo "Your browser does not support iframe videos";
-							} ?>
-							</div>
-							<?php } ?>
+					else {
+						echo "Your browser does not support iframe videos";
+					} ?>
+				</div>
+			<?php } ?>
 <!--================================================================================================================
 												iFrame support ends here :D
-================================================================================================================ -->
-						</div>
-						<div class="row">
-						       <?php foreach ($media_gallery as  $value_image) {	?>
-									<div class="col-sd-4">
-										<?php	
-										echo "<br>";
-										echo wp_get_attachment_image( $value_image, array(500,500));
-										echo "<br>";
-										?> 
-									</div> 
-								<?php } ?>
-							</div>
-						</div>
-						</div>
-				</div>
-		<?php endwhile; 
-endif;?>
+=================================================================================================================-->
+			</div>
+			<div class="row">
+			<?php foreach ($media_gallery[0] as  $value_image) {	?>
+				<div class="col-sd-4">
+					<?php echo wp_get_attachment_image( $value_image, array(500,500)); ?> 
+				</div> 
+			<?php } ?>
+			</div>
+		
+		</div><!-- container ends here -->
+	
+	</div><!-- media_photo ends here -->
+
+</div><!-- media_holder ends here -->
+		
+<?php endwhile; endif;?>
 
 <?php get_footer(); ?>
