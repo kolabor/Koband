bf4a594932e5b9b975836e44797837f636b80277
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-04-27 17:09:20 +0200

    r 27.04

--- search.php
+++ search.php
@@ -7,31 +7,7 @@
  */
 
 get_header(); ?>
-<<<<<<< HEAD
 
-
-            <?php if ( have_posts() ) : ?>
-
-                
-            <div class="container search-holder">
-                <header class="page-header">
-     <h1 class="page-title"><?php printf( _e( 'Search Results for: %s', 'koband' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-
-                </header><!-- .page-header -->
-                <div class="row koband_post_news">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <div class="col-md-4">
-                        <div class="card mb-4 box-shadow">
-                            <div class="news-title main_font_color"><h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2></div>
-                            <a class="card-img-top" href="<?php the_permalink();?>"><?php the_post_thumbnail(array(300,300)); ?></a>
-                            <div class="card-body">
-                                <div id="card-text" class="main_font_color"><?php the_excerpt(); ?></div>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <span  class="btn btn-sm btn-outline-secondary read_more"><a href="<?php the_permalink();?>"><?php _e('READ MORE →', 'koband');?></a></span>
-                                        </div>
-                                    </div>
-=======
 <div class="container search-holder">
     <header class="page-header">
         <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'shape' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
@@ -49,7 +25,7 @@ get_header(); ?>
                                 <div class="btn-group">
                                     <span  class="btn btn-sm btn-outline-secondary read_more"><a href="<?php the_permalink();?>"><?php _e('READ MORE →', 'koband');?></a></span>
                                 </div>
->>>>>>> 2223cbf113d73345db4873bd3960d76b307099a6
+
                             </div>
                     </div>
                 </div>
