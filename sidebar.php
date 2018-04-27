b2b6035497701081b00e0d44d6291072ca78618c
theados <admir.ibraimi@kolabor.net>
2018-04-27 13:47:59 +0200

    ado

--- sidebar.php
+++ sidebar.php
@@ -14,4 +14,9 @@
  do_action( 'before_sidebar' ); 
  ?>
 <div class="sidebar" role="complementary">
-<p><?php _e('Side-Bar', 'koband');?></p></div>
+<p><?php _e('Side-Bar', 'koband');?></p>
+
+<?php if ( is_active_sidebar( 'ko_band_sidebar_one' ) ) : ?>
+              	<?php dynamic_sidebar( 'ko_band_sidebar_one' ); ?> <!-- #second .widget-area-->
+            <?php endif; ?>
+      </div>
