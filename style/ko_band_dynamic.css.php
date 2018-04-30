e3f27f72dca5d2a842b4196de92c75d300603284
Axheladini <agonxheladini001@gmail.com>
2018-04-30 09:48:40 +0200

    Before pulling

--- style/ko_band_dynamic.css.php
+++ style/ko_band_dynamic.css.php
@@ -101,21 +101,19 @@ background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
 
 
 /* Menu item colors */
-
-body
+.home, .single, .page, .default
 {
   
   font-size:  <?php echo $koband_theme_font_size; ?> !important;
   line-height:  <?php echo $koband_theme_line_height; ?> !important;
-  color:  <?php if(isset($koband_main_font_color)) { if ($koband_main_font_color > 0) { echo $koband_main_font_color;} else {echo "yellow"; } }?> !important;
+  color: <?php echo $koband_main_font_color; ?> !important;
 }
 
-h1, h2, h3 
+.home h1, .home h2 .home h3, .home h4, .single h1, .single h2 .single h3, .single h4, .page h1, .page h2 .page h3, .page h4,
 {
   font-family: <?php echo $koband_headding_font; ?> !important;
-  color: <?php if(isset($koband_main_font_color)) { if ($koband_main_font_color > 0) { echo $koband_main_font_color;} else {echo "yellow"; } }?> !important;
+  color: <?php echo $koband_main_font_color; ?> !important;
   font-size:  <?php echo $koband_theme_font_size; ?> !important;
-
 }
 
 #News {
@@ -164,7 +162,7 @@ background-color: <?php echo  $koband_gallery_background_color; ?> !important;
 }
 
 .main_font_color{
-  color:  <?php if(isset($koband_main_font_color)) { if ($koband_main_font_color > 0) { echo $koband_main_font_color;} else {echo "yellow"; } }?> !important;
+  color: color: <?php echo $koband_main_font_color; ?> !important;  
 }
 
 .font-line_height {
