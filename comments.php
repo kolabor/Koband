536e381de76b8ee74840bd174139a056c6d3a6d7
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-04-30 10:30:58 +0200

    rilinda 30.04.18

--- comments.php
+++ comments.php
@@ -40,13 +40,13 @@ get_header(); ?>
                         // Are there comments to navigate through?
                         if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :?>
                     <nav class="navigation comment-navigation" role="navigation">
-                        <h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'koband' ); ?></h1>
-                        <div class="nav-previous"><?php previous_comments_link( _e( '&larr; Older Comments', 'koband' ) ); ?></div>
-                        <div class="nav-next"><?php next_comments_link( _e( 'Newer Comments &rarr;', 'koband' ) ); ?></div>
+                        <h1 class="screen-reader-text section-heading"><?php echo__( 'Comment navigation', 'koband' ); ?></h1>
+                        <div class="nav-previous"><?php previous_comments_link( echo__( '&larr; Older Comments', 'koband' ) ); ?></div>
+                        <div class="nav-next"><?php next_comments_link( echo__( 'Newer Comments &rarr;', 'koband' ) ); ?></div>
                     </nav><!-- .comment-navigation -->
                     <?php endif; // Check for comment navigation ?>
                     <?php if ( ! comments_open() && get_comments_number() ) : ?>
-                    <p class="no-comments"><?php _e( 'Comments are closed.' , 'koband' ); ?></p>
+                    <p class="no-comments"><?php echo__( 'Comments are closed.' , 'koband' ); ?></p>
                     <?php endif; ?>
                 <?php endif; // have_comments() ?>
                 <?php comment_form(); ?>  
