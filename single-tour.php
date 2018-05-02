32fcf6b9bd254d2c911d7b80018accb7b0509990
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-05-02 12:40:07 +0200

    fisnik

--- single-tour.php
+++ single-tour.php
@@ -10,3 +10,10 @@
  */
 
 get_header('noscroll'); ?>
+<h1>testttt</h1> <?php 
+if (have_posts() ) : 
+	
+ 	//start loop ?>
+	<?php  while ( have_posts() ) : the_post(); 
+	$post_id = get_the_ID(); ?>
+	<?php endwhile; endif;?>
