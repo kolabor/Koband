11b92022ec326e8051eec0fadd5dafad53071274
theados <admir.ibraimi@kolabor.net>
2018-05-04 14:29:51 +0200

    ado

--- single-media.php
+++ single-media.php
@@ -17,10 +17,10 @@ get_header('noscroll'); ?>
 if (have_posts() ) : 
 	
  	//start loop ?>
-<div class='media_holder'>
+
 	<?php  while ( have_posts() ) : the_post(); 
 	$post_id = get_the_ID(); ?>
-		<div class="container">
+<div class="container">
 		<div id="media-photo"><?php the_post_thumbnail('single_news_thumb'); ?></div>
 		<h1><div id="single-media-title"><?php the_title();?></div></h1>
 			<div class="row">
@@ -39,16 +39,20 @@ if (have_posts() ) :
 	<?php 
 	$media_gallery = get_post_meta($post_id, 'vdw_gallery_id', false);
 	$media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); ?>
+	<div class="col-md-12">
+		<div class="row">
 			
+
 			<?php 
+
 				if (isset($media_video_gallery[0])){
 				foreach ($media_video_gallery[0] as  $value_video_gallery) { ?>
-				<div class="col-md-12">
-					<div class="row">
-						<div class="gal">
+				
+					
 <!--================================================================================================================
 						// Checking which iframe to use from the select box at backed :)
-=================================================================================================================--> 	
+=================================================================================================================-->
+					
 		            <?php
 		            $video_type = $value_video_gallery['select'];
 					if(isset($video_type) && $video_type == "option1"){
@@ -56,7 +60,7 @@ if (have_posts() ) :
 						$value_video_gallery['link'] = substr($data, strpos($data, "v=") + 2);	
 						$value_video_gallery_video['link'] = 'https://www.youtube.com/embed/' . $value_video_gallery['link'];
 						$value_video_gallery_image['link'] = 'https://img.youtube.com/vi/' . $value_video_gallery['link']; ?>
-						<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>"></iframe> 
+						<!--<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>"></iframe>--> 
 						<img src="<?php echo $value_video_gallery_image['link']?>/hqdefault.jpg" alt="Smiley face" height="265" width="370"> 		
 					<?php }
 
@@ -65,7 +69,7 @@ if (have_posts() ) :
 						$value_video_gallery['link'] = substr($data, strrpos($data, "/") +1);
 						$value_video_gallery_video['link'] = 'https://player.vimeo.com/video/' . $value_video_gallery['link']; 
 						$value_video_gallery_image['link'] = 'http://vimeo.com/api/v2/video/' . $value_video_gallery['link']; ?>
-						<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+						<!--<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>-->
 						<img src="<?php echo $value_video_gallery_image['link']?>.json" alt="Smiley face" height="265" width="370">
 
 					<?php }
@@ -75,17 +79,17 @@ if (have_posts() ) :
 						$value_video_gallery['link'] = substr($data, strpos($data, "video/") + 6);
 						$value_video_gallery_video['link'] = '//www.dailymotion.com/embed/video/' . $value_video_gallery['link'];
 						$value_video_gallery_image['link'] = '//www.dailymotion.com/thumbnail/video/' . $value_video_gallery['link']; ?>
-						<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>"></iframe>
+						<!--<iframe width="370" height="265" src="<?php echo $value_video_gallery_video['link']?>"></iframe>-->
 						<img src="<?php echo $value_video_gallery_image['link']?>" alt="Smiley face" height="265" width="370">
 					<?php }
 
 					else {
 						echo "<?php echo __('Your browser does not support this type of iframe videos', 'koband');?>";
 					} ?>
-				</div>
-				</div>
+				
 			<?php } ?>
-			</div>
+		</div>
+	</div>
 			<?php } ?>
 <!--================================================================================================================
 												iFrame support ends here :D
@@ -103,11 +107,8 @@ if (have_posts() ) :
 			
 			</div>
 		
-		</div><!-- container ends here -->
+</div><!-- container ends here -->
 	
-	</div><!-- media_photo ends here -->
-
-</div><!-- media_holder ends here -->
 		
 <?php endwhile; endif;?>
 
