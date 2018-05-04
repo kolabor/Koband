15367e1a9889749a2365ff691ba3f85844ef57e4
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-05-04 14:21:41 +0200

    r 04.05.18

--- default-page.php
+++ default-page.php
@@ -2,7 +2,7 @@
 /**
  * 
  *
- * Template Name: Default Template
+ * Template Name: Default Template 
  *
  *
  *
@@ -12,7 +12,7 @@
  * @since Koband 1.0
  */?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
@@ -26,5 +26,6 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+</div><!-- #post-## -->
+
 
