09a48306ad53706063b4111bccfb95b02210d976
rilinda1987 <rilinda.jahiu@kolabor.net>
2018-05-09 17:11:02 +0200

    rilinda 08.05.18

--- custom/templates/video-slider-template.php
+++ custom/templates/video-slider-template.php
@@ -28,22 +28,21 @@
 		
 		$data = $slide_video_link;
 		$slide_video_link = substr($data, strpos($data, "v=") + 2);
-		$slide_video_link = 'https://www.youtube.com/embed/' . esc_html($slide_video_link); ?>
+		$slide_video_link = 'https://www.youtube.com/embed/' . $slide_video_link; ?>
 
 	<?php endwhile; ?>
 	<!--loop ends here-->
-		<div class="video-bg"><input placeholder="http://somelink.com" />
+		<div class="video-bg">
 				<div class="iframe-wrapper">
-					<iframe  src="<?php echo esc_url($slide_video_link);?>?autoplay=1&loop=1&mute=1" ></iframe>
-				</div>		
+					<iframe  src="<?php echo esc_url($slide_video_link)?>?autoplay=1&controls=0&loop=auto&mute=1&start=10&end=30&rel=0&playlist=1" ></iframe>
+				</div>	
 		</div>		
 		<div class="sl-content">
 			    <h5 class="slider_text_color"><?php if(isset($slide_video_title[0])) { echo esc_attr($slide_video_title); } ?></h5>
 			    <p class="slider_text_color font-line_height"><?php if(isset($slide_video_subtitle[0])) 	{ echo  esc_attr($slide_video_subtitle); } ?></p>
-			    <a class="btn btn-lg slider_button_text_color" href="<?php if(isset($slide_video_buttonlink)){ 
-			    	echo  esc_url($slide_video_buttonlink[0]); } ?>">
+			    <a class="btn btn-lg slider_button_text_color" href="<?php if(isset($slide_video_buttonlink[0])){echo esc_url($slide_video_buttonlink); } ?>">
 			    <?php if(isset($slide_video_buttontitle[0])){ 
-			   	echo  esc_attr($slide_video_buttontitle); } ?></a>
+			   	echo esc_attr($slide_video_buttontitle); } ?></a>
 		</div>
 <?php endif;
 ?>
