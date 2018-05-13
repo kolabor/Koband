564ab543c2c57ff06fdfa66588459e17f7eccc32
Axheladini <agonxheladini001@gmail.com>
2018-05-13 19:13:06 +0200

    Before pulling

--- admin/ko_band_admin.js
+++ admin/ko_band_admin.js
@@ -352,37 +352,35 @@ jQuery(document).ready(function( $ ){
 
 jQuery(window).load(function ($) {
 
-  /*Show hide slider attribues based on slider type*/
-  wp.customize.control('ko_band_home_page_slider_type', function( control ) 
+ var customize = this; 
+
+  customize.control('ko_band_home_page_slider_type', function(control) 
    {       
              
              var slider_type = control.settings.default._value;
              if(slider_type === "Image")
              {  
-                     wp.customize.control('ko_band_home_page_slider_videolink').deactivate();
-                     wp.customize.control('ko_band_home_page_slider_title').deactivate();
-                     wp.customize.control('ko_band_home_page_slider_text').deactivate();
-                     wp.customize.control('ko_band_home_page_slider_buttontitle').deactivate();
-                     wp.customize.control('ko_band_home_page_slider_buttonlink').deactivate();
+                     customize.control('ko_band_home_page_slider_videolink').deactivate();
+                     customize.control('ko_band_home_page_slider_title').deactivate();
+                     customize.control('ko_band_home_page_slider_text').deactivate();
+                     customize.control('ko_band_home_page_slider_buttontitle').deactivate();
+                     customize.control('ko_band_home_page_slider_buttonlink').deactivate();
              }
              else if(slider_type === "Video")
              {
-                     wp.customize.control('ko_band_home_page_slider_videolink').activate();
-                     wp.customize.control('ko_band_home_page_slider_title').activate();
-                     wp.customize.control('ko_band_home_page_slider_text').activate();
-                     wp.customize.control('ko_band_home_page_slider_buttontitle').activate();
-                     wp.customize.control('ko_band_home_page_slider_buttonlink').activate();
+                     customize.control('ko_band_home_page_slider_videolink').activate();
+                     customize.control('ko_band_home_page_slider_title').activate();
+                     customize.control('ko_band_home_page_slider_text').activate();
+                     customize.control('ko_band_home_page_slider_buttontitle').activate();
+                     customize.control('ko_band_home_page_slider_buttonlink').activate();
              }
 
    });
   /*Show hide slider attribues based on slider type ends*/
    
-   wp.customize.control('ko_band_first_render_modules', function( control ) 
+   customize.control('ko_band_first_render_modules', function( control ) 
    {
         var first_moduel_value = control.settings.default._value;
-
-        $('#_customize-input-ko_band_second_render_modules option[value="'+first_moduel_value+'"]').remove();
-        //$('#_customize-input-ko_band_second_render_modules').find('option[value="'+first_moduel_value+'"]').hide();
 
    });
 
@@ -390,8 +388,12 @@ jQuery(window).load(function ($) {
 });
 jQuery(function($) {
 
- wp.customize.bind( 'ready', function() {
-   var customize = this;
+ 
+
+   wp.customize.bind('ready', function() {
+  
+    var customize = this;
+
        customize.control('ko_band_home_page_slider_type', function( control ) {       
              
              var slider_type = control.settings.default._value;
@@ -423,7 +425,7 @@ jQuery(function($) {
                });
                
        });
-  });
+  }); 
 
 });
 /*WP Customizer section ends here*/
