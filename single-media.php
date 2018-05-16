911ae285875c8bc6ba770a11ad906c947e02fdeb
Axheladini <agonxheladini001@gmail.com>
2018-05-16 11:01:30 +0200

    Fixed if isset

--- single-media.php
+++ single-media.php
@@ -45,29 +45,33 @@ if (have_posts() ) :
 	    $media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); 
        
       $all_gallery_items = array();
-      if(isset($media_gallery[0], $media_video_gallery[0])) {
-      $count_images = count($media_gallery[0]);
-      $count_videos = count($media_video_gallery[0]);
 
-      /*Declaring an array that will hold all gallery items*/
+      if (isset($media_gallery[0]))
+      {
+        $count_images = count($media_gallery[0]);
+        /*Insert images to the all_gallery_items array*/
+        if($count_images > 0)
+        {
+          for($i=0; $i < $count_images; $i++)
+          {
+            array_push($all_gallery_items, array("type" => "image",  "videotype" => "none", "link"=> $media_gallery[0][$i] ));
+          }
+        } 
+      }
 
-      /*Insert images to the all_gallery_items array*/
-      if($count_images > 0)
+      if(isset($media_video_gallery[0])) 
       {
-        for($i=0; $i < $count_images; $i++)
+      
+        $count_videos = count($media_video_gallery[0]);
+        
+        if($count_videos > 0)
         {
-        	array_push($all_gallery_items, array("type" => "image",  "videotype" => "none", "link"=> $media_gallery[0][$i] ));
-        }
-      } 
-      /*Insert videos to the all_gallery_items array*/
-      if($count_videos > 0)
-      {
-        for($v=0; $v < $count_videos; $v++)
-        {
-        	array_push($all_gallery_items, array("type" => "video",  "videotype" =>$media_video_gallery[0][$v]['select'], "link"=> $media_video_gallery[0][$v]['link'] ));
+          for($v=0; $v < $count_videos; $v++)
+          {
+          	array_push($all_gallery_items, array("type" => "video",  "videotype" =>$media_video_gallery[0][$v]['select'], "link"=> $media_video_gallery[0][$v]['link'] ));
+          }
         }
       }
-    }
      
      /*Randomize Gallery videos and images*/
      shuffle($all_gallery_items);
