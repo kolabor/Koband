ce0e099550991b25ae797b59512be05eae70b525
Axheladini <agonxheladini001@gmail.com>
2018-05-16 10:01:54 +0200

    Fix issue

--- single-media.php
+++ single-media.php
@@ -42,7 +42,7 @@ if (have_posts() ) :
      <?php 
 
       $media_gallery = get_post_meta($post_id, 'vdw_gallery_id', false);
-	  $media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); 
+	    $media_video_gallery = get_post_meta($post_id, 'ko_band_repetable_video_field', false); 
 
       $count_images = count($media_gallery[0]);
       $count_videos = count($media_video_gallery[0]);
@@ -51,15 +51,20 @@ if (have_posts() ) :
       $all_gallery_items = array();
       
       /*Insert images to the all_gallery_items array*/
-      for($i=0; $i < $count_images; $i++)
+      if($count_images > 0)
       {
-      	array_push($all_gallery_items, array("type" => "image",  "videotype" => "none", "link"=> $media_gallery[0][$i] ));
+        for($i=0; $i < $count_images; $i++)
+        {
+        	array_push($all_gallery_items, array("type" => "image",  "videotype" => "none", "link"=> $media_gallery[0][$i] ));
+        }
       }
-      
       /*Insert videos to the all_gallery_items array*/
-      for($v=0; $v < $count_videos; $v++)
+      if($count_videos > 0)
       {
-      	array_push($all_gallery_items, array("type" => "video",  "videotype" =>$media_video_gallery[0][$v]['select'], "link"=> $media_video_gallery[0][$v]['link'] ));
+        for($v=0; $v < $count_videos; $v++)
+        {
+        	array_push($all_gallery_items, array("type" => "video",  "videotype" =>$media_video_gallery[0][$v]['select'], "link"=> $media_video_gallery[0][$v]['link'] ));
+        }
       }
      
      /*Randomize Gallery videos and images*/
