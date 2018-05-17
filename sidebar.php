dd747a34b821dea4fdd5a3e42421d1e6e0e17b49
Fisnik Ajredini <fisnik.ajredini@kolabor.net>
2018-05-17 12:28:24 +0200

    fisnik

--- sidebar.php
+++ sidebar.php
@@ -14,10 +14,11 @@
  do_action( 'before_sidebar' ); 
  ?>
 <div class="sidebar" role="complementary">
-
+	<div class="sidebar_content">
 
  	<?php if ( is_active_sidebar( 'ko_band_sidebar' ) ) : ?>
         <?php dynamic_sidebar( 'ko_band_sidebar' ); ?><!-- #sidebar-area -->
     <?php endif; ?>
-
+	
+	</div>
 </div>
